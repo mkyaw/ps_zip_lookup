46e337c4c471516ca15c5a1e08bdf53c659b3b12
Myo Kyaw <myotkyaw@gmail.com>
2019-12-07 09:57:45 +0630

    Comment out unused railties

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,13 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
 require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
+
+# Not using these railties
+# require "active_storage/engine"
+# require "action_mailer/railtie"
+# require "action_view/railtie"
+# require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
