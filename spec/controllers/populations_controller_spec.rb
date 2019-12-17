22edad6ea1ab2525a6ed9eab215126d009214f9a
Myo Kyaw <myotkyaw@gmail.com>
2019-12-17 08:09:57 +0630

    Return error when zip is not passed

--- spec/controllers/populations_controller_spec.rb
+++ spec/controllers/populations_controller_spec.rb
@@ -3,35 +3,47 @@ require 'rails_helper'
 RSpec.describe PopulationsController, type: :controller do
 
   describe 'GET #index' do
-    let(:populations) do
-      {
-        zip: '12345',
-        cbsa: '3111',
-        msa: 'Los Angeles-Long Beach-Glendale, CA',
-        pop2015: '10170292',
-        pop2014: '10109436'
-      }
+    describe 'successful call' do
+      let(:populations) do
+        {
+          zip: '12345',
+          cbsa: '3111',
+          msa: 'Los Angeles-Long Beach-Glendale, CA',
+          pop2015: '10170292',
+          pop2014: '10109436'
+        }
+      end
+
+      before do
+        expect(GetPopulations).to receive(:call)
+          .and_return(populations)
+      end
+
+      it 'returns http success' do
+        get :index, params: { zip: '12345' }
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns correct data' do
+        get :index, params: { zip: '12345' }
+        expect(JSON.parse(response.body)).to eq(
+          'zip' => '12345',
+          'cbsa' => '3111',
+          'msa' => 'Los Angeles-Long Beach-Glendale, CA',
+          'pop2015' => '10170292',
+          'pop2014' => '10109436'
+        )
+      end
     end
 
-    before do
-      expect(GetPopulations).to receive(:call)
-        .and_return(populations)
-    end
-
-    it 'returns http success' do
-      get :index, params: { zip: '12345' }
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'returns correct data' do
-      get :index, params: { zip: '12345' }
-      expect(JSON.parse(response.body)).to eq(
-        'zip' => '12345',
-        'cbsa' => '3111',
-        'msa' => 'Los Angeles-Long Beach-Glendale, CA',
-        'pop2015' => '10170292',
-        'pop2014' => '10109436'
-      )
+    describe 'missing parameter' do
+      it 'returns the error' do
+        get :index
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to eq(
+          'error' => 'param is missing or the value is empty: zip'
+        )
+      end
     end
   end
 end
